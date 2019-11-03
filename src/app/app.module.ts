9cb48c3f5794cc7d61dbf0f807a9fcc689cbc26a
MUTAI <yegoallen@gmail.com>
2019-11-03 23:55:17 +0300

    this commit adds constructors to user class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
+    HomeComponent,
+    
     
     
   ],
---
+++ src/app/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    constructor(
+        public avatar_url:any,
+        public login:any,
+        public company: any,
+        public location: any,
+        public  name: any,
+        public followers: any,
+        public following: any,
+        public created_at: Date,
+        public public_repos: number,
+        public html_url: any,
+      ){}
+}
